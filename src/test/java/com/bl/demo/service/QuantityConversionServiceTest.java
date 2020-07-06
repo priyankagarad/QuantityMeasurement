c0ddd2b2ff3b77d65630d65b3cebc66777af8b38
priyankagarad <garadpriyanka1995@gmail.com>
2020-07-06 19:36:42 +0530

    ADDED: Test Case For Check Count Of SubUnit Of Length

--- src/test/java/com/bl/demo/service/QuantityConversionServiceTest.java
+++ src/test/java/com/bl/demo/service/QuantityConversionServiceTest.java
@@ -1,10 +1,12 @@
 package com.bl.demo.service;
+import com.bl.demo.enumeration.Quantity;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import static sun.jvm.hotspot.runtime.BasicObjectLock.size;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = QuantityConversionService.class)
@@ -19,4 +21,12 @@ public class QuantityConversionServiceTest {
         Assert.assertEquals(4,size);
     }
 
+    @Test
+    public void givenQunatityUnit_whenCountInQuantityUnitList_shouldRequiredCountOfQuantityUnit(){
+        int size = quantityConversionService.getListOfQuantityUnits(Quantity.LENGTH).size();
+        Assert.assertEquals(4,size);
+    }
 }
+
+
+
